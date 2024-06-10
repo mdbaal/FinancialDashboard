51f75e7bb574f8f418c1c3af8e488cbe7a355c7a
Mirco Baalmans <mircobaalmans@live.nl>
2024-06-10 10:53:55 +0200

    Account page

--- resources/views/accounts/account.blade.php
+++ resources/views/accounts/account.blade.php
@@ -1,3 +1,22 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+<x-layout>
+    <x-slot:title>{{$account->id ?? 'Account -'}}</x-slot:title>
+    <h1 class="w-full text-3xl">Account</h1>
+
+
+    <div class="w-full drop-shadow-xl border-2 border-gray-200 rounded p-2">
+        <table class="w-full text-md text-left">
+            <thead class="text-gray-700 uppercase bg-gray-50">
+            <tr>
+                <th>ID</th>
+                <th>Transaction</th>
+                <th>Amount</th>
+                <th>Description</th>
+                <th>Edit</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+</x-layout>
